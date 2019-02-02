547a677ba8d0433a2ef409a006f66ddffc98b407
Michael Uskov <uskovm@gmail.com>
2019-02-02 15:51:21 +0500

    Valid hash

--- src/access.js
+++ src/access.js
@@ -2,12 +2,10 @@ const crypto = require('crypto');
 const config = require("config");
 
 module.exports.checkAccess = (req, res) => {
-    if (process.env.NODE_ENV !== "production")
-        return true;
     const params = req.method.toUpperCase() === "GET" ? req.query : req.body;
     if (config.get("adminHash") && params.hash === config.get("adminHash"))
         return true;
-    const url = `https://api.zmh.wtf/${params.quiz}`;
+    const url = `https://zmh.wtf/${params.quiz}`;
     const md5Str = `${url};${decodeURIComponent(params.login)};${config.get("hashSalt")}`;
     const trueHash = crypto.createHash('md5').update(md5Str).digest("hex");
     if (params.hash === trueHash)
